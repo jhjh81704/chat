8781370e9cef5369db8f37536f521f5173ecae42
jhjh81704 <jhjh81704@gmail.com>
2019-05-03 22:07:14 +0800

    Done with read.py

---
+++ readfile.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri May  3 21:28:38 2019
+
+@author: user
+"""
+
+def read_file(filename):
+    lines = []
+    with open(filename, 'r', encoding= 'utf-8-sig') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+def convert(lines):
+    new = []
+    person = None
+    for line in lines:
+        if line == 'Allen':
+            person = 'Allen'
+            continue
+        elif line == 'Tom':
+            person = 'Tom'
+            continue
+        if person:
+            new.append(person + ': ' + line)
+        print(line)
+    return new
+
+def write_file(filename, lines):
+    with open(filename, 'w') as f:
+        for line in lines:
+            f.write(line + '\n')
+        
+        
+def main():
+    lines = read_file('input.txt')
+    lines = convert(lines)
+    write_file('output.txt', lines)
+
+
+main() # 執行程式的進入點
